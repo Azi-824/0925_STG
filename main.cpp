bfe6c2825b95325a69df9da3455bb8f549642fc2
hj-g18005 <hj-g18005@sist.ac.jp>
2019-09-26 14:40:33 +0900

    IMAGE.hppとIMAGE.cppを追加

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
+//************ ヘッダーファイル読み込み ************
 #include "DxLib.h"
+#include "IMAGE.hpp"
+
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	int i;
+
+
 
 	return 0;
 }
